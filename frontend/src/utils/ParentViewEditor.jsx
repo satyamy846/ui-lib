fb9d4dba7cb8e3b4faa395ea8000496da4e72b65
Satyam Kumar <satyadav941@gmail.com>
2025-05-25 16:30:09 +0530

    scroll bar removed

--- frontend/src/utils/ParentViewEditor.jsx
+++ frontend/src/utils/ParentViewEditor.jsx
@@ -5,7 +5,7 @@ import CodeEditor from "./CodeEditor";
 const ParentViewEditor = ({ renderPreview, fileName, folderName }) => {
     return (
         <>
-            <div className="p-4 max-w-4xl mx-auto break-words whitespace-pre-wrap">
+            <div className="p-4 max-w-4xl mx-auto break-words whitespace-pre-wrap parent-view-editor-container">
                 <CodeEditor renderPreview={renderPreview} fileName={fileName} folderName={folderName} />
             </div>
         </>
